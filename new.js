fe9f66e9e45ea4ff071a756cf66a37a7100bf73d
Sagar R Bandagar <sagarbandagar18@gmail.com>
2024-06-24 17:05:57 +0530

    new chapters are added

---
+++ new.js
@@ -0,0 +1,16 @@
+//chapter 03
+// difference between let , var and const
+let a = 10;
+let b = 20;
+const c=100  // const cant be updated and cant be re-declared
+
+console.log(a + b)
+{
+    let a = 100    // var can be updated and also can be redeclared
+    console.log(a)  // let can be updated but not re-declared
+}
+
+let author='Sagar'
+console.log(author)
+
+// Chapter 04
