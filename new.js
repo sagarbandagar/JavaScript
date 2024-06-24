4d4f40e8d5b28d0a0a253297144995aa735c2fc4
Sagar R Bandagar <sagarbandagar18@gmail.com>
2024-06-24 17:41:22 +0530

    New Chapters are added

--- new.js
+++ new.js
@@ -14,3 +14,24 @@ let author='Sagar'
 console.log(author)
 
 // Chapter 04
+// Primitive Data Types in Js are : NNBBSSU 
+let null_exp= null ;// null
+let num_exp=345;   // number
+let boo_exp=true;   // boolean
+let big_int=BigInt('11223344') // big int
+let Symbol_exp=Symbol('XYZ') // symbol
+let string_exp='Sagar'
+let undefined_exp=undefined;
+
+console.log(null_exp,num_exp,boo_exp,big_int,Symbol_exp,string_exp,undefined_exp)
+
+//objects in Js are Non-Primitive Data types
+// objects are in key:value pairs
+
+const user ={
+    'Name':'Sagar',
+    'Role':'DevOps Engineer',
+    'YOE': 1
+}
+
+console.log(user)
