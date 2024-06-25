7c4e6ba7c4439110b79d599d90ebfc97bcf381a7
Sagar <sagarbandagar18@gmail.com>
2024-06-25 23:23:25 +0530

    added ternary operator

--- operators.js
+++ operators.js
@@ -1,6 +1,8 @@
 // Chapter 06
 // Expressions and operators
 
+const prompt = require('prompt-sync')();  // to initialize a prompt
+
 let a = 100
 let b = 25
 console.log('a+b is :', a + b)
@@ -10,4 +12,9 @@ console.log('a/b is :', a / b)
 console.log('a%b is :', a % b)
 console.log('a &&b is :', a && b)
 
-console.log('New message ');
+
+
+// Ternary operator
+const age =prompt("Enter your age");
+Number.parseInt(age);
+console.log("Hey You are ", (age < 18) ? 'Not eligible to vote' : 'eligible to vote');
