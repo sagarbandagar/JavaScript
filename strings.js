6c73084e6933ce9f7b0ddb7d53892c44ed531e98
Sagar R Bandagar <sagarbandagar18@gmail.com>
2024-06-26 16:27:23 +0530

    new string chapter is added

---
+++ strings.js
@@ -0,0 +1,11 @@
+// Chapter 04 
+// Strings in Javascript
+
+let  name ="Rama";
+ans=`Name of the Boy is ${name} and length is ${name.length}` //string interpolation
+console.log(ans); //string interpolation
+
+// escape sequence in js
+
+user="Marcus \D'Raman;"
+console.log(user);
