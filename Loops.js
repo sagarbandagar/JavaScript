0b6ef145f0eba100dd539050b686ef5c0d81e32a
Sagar R Bandagar <sagarbandagar18@gmail.com>
2024-06-28 11:20:28 +0530

    new array methods

--- Loops.js
+++ Loops.js
@@ -1,21 +1,15 @@
-//Chapter 05
-//Loops in JS 
+// // //Chapter 05
+// // //Loops in JS 
 
 let arr = [1, 2, 3, 4, 5]
 
 for (let i = 0; i < arr.length; i++) {
     console.log(arr[i]);
-} // regular method that we use
+}
+// regular method that we use
 
 let ans = Array.from(arr)
 console.log(ans);   // to convert other objects into array of js
-
-//for each loop in js
-arr.forEach((element) => {
-    console.log(element * element);
-}
-)
-
 
 // alternate method
 let array = [100, 200, 300, 400]
@@ -26,3 +20,23 @@ for (let i in array) {
 
 
 
+const new_arr = [5, 10, 15, 20]
+
+new_arr.forEach((value, index, array) => {
+    console.log(value, index, array)
+})
+
+// map in array 
+let a =[500,550,600,650]
+let newArray =a.map((value)=>{
+    return value + value;
+})
+console.log(newArray);
+console.log(a);
+
+// Filter in array
+let filter_array=a.filter((value)=>{
+    return value < 600
+})
+console.log(filter_array)
+console.log(a)
