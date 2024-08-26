6f0ed8ffc2f37f9a0176b11af5bd90f5ff7a5f7c
Sagar R Bandagar <sagarbandagar18@gmail.com>
2024-08-26 13:00:01 +0530

    added to 01_basics

--- strings.js
+++ strings.js
@@ -1,8 +1,8 @@
 // Chapter 04 
 // Strings in Javascript
 
-let name = "Rama";
-ans = `Name of the Boy is ${name} and length is ${name.length}` //string interpolation
+let Username = "Rama";
+ans = `Name of the Boy is ${Username} and length is ${Username.length}` //string interpolation
 console.log(ans); //string interpolation
 
 // escape sequence in js
@@ -12,12 +12,12 @@ console.log(user);
 
 // quiz for printing a name using for loop
 
-for (let i = 0; i < name.length; i++) {
+for (let i = 0; i < Username.length; i++) {
 
-    console.log(name[i]);
+    console.log(Username[i]);
 }
-console.log(name.toUpperCase()); // UpperCase
-console.log(name.toLowerCase()); //LowerCase
-console.log(name.slice(1)); //  slicing
-console.log(name.trim());  // trimming , removing white spaces
-console.log(name.concat(' is a boy but less than 18 '));  // concatenation
+console.log(Username.toLowerCase()); //LowerCase
+console.log(Username.slice(1)); //  slicing
+console.log(Username.trim());  // trimming , removing white spaces
+console.log(Username.concat(' is a boy but less than 18 '));  // concatenation
+console.log(Username.toUpperCase()); // UpperCase
