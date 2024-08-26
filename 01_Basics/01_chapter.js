8e1b474db416f42509bb8397488cdf7d38873855
Sagar R Bandagar <sagarbandagar18@gmail.com>
2024-08-26 13:20:47 +0530

    added

--- 01_Basics/01_chapter.js
+++ 01_Basics/01_chapter.js
@@ -1,23 +1,4 @@
-////practice set for chapter 01
-//// create a variable for storing the string and try to add a number to it
-//let str = 'Some String'
-//let num = 10
-//console.log(str + num)
-//
-////checking the type of
-//console.log(typeof (str + num))
-//
-//// adding number to the object
-//
-//const item = {
-//    name: 'some',
-//    class: 'B',
-//    isPrincriple: false
-//}
-//
-//item['last'] = 'thing'
-//item['name'] = 'new'
-//console.log(item)
+
 const id = 12345;
 let Username = "Sagar Bandagar";
 let email ="sagar@airowire.com";
