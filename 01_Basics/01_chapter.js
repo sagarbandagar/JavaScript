1e395ced20d2ddcee3b9c52938bbf4a2efacca72
Sagar R Bandagar <sagarbandagar18@gmail.com>
2024-08-26 15:08:04 +0530

    added some other functions

--- 01_Basics/01_chapter.js
+++ 01_Basics/01_chapter.js
@@ -1,9 +1,9 @@
 
 const id = 12345;
 let Username = "Sagar Bandagar";
-let email ="sagar@airowire.com";
-let city="Bangalore";
+let email = "sagar@airowire.com";
+let city = "Bangalore";
 var somecity;
-// alert(3+3)
-console.table([id,Username,email,city,somecity]);
+
+console.table([id, Username, email, city, somecity]);
 
--- 01_Basics/05_Comparison.js
+++ 01_Basics/05_Comparison.js
@@ -1,10 +1,21 @@
-console.log("2" > 1);  //comparing string with a number converts string to the number
-console.log(1 > "2");
+// console.log("2" > 1);  //comparing string with a number converts string to the number
+// console.log(1 > "2");
 
-console.log(null >= 0);  
-console.log(null == 0);  // false
-console.log(null < 0);   // false
-console.log(typeof null);  //an object 
+// console.log(null >= 0);
+// console.log(null == 0);  // false
+// console.log(null < 0);   // false
+// console.log(typeof null);  //an object 
+
+
+// difference between == and === operators
+
+let a = 100
+let b ="100"
+console.log(a == b);
+console.log(a === b);  // checks for datatype
+// console.log(a == b);
+// console.log(a == b);
+
 
 
 
